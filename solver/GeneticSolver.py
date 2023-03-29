e61ce7d2889c485034d3acb02b50fbf10a2d2e5a
Zedrick Serson <18zws@queensu.ca>
2023-03-29 15:39:38 -0400

    Add more computation to the MS algorithm

--- solver/GeneticSolver.py
+++ solver/GeneticSolver.py
@@ -1,3 +1,7 @@
+import random
+from Register import Register
+from Instruction import *
+
 class GeneticSolver:
 	'''
 	An implementation of a linear GP tree,
@@ -6,4 +10,64 @@ class GeneticSolver:
 	
 	def __init__(self):
 		self.registers = {}
+		self.nextConstantRegister = 0
+		self.nextInputRegister = 0
+		self.nextVariableRegister = 0
+		self.nextOutputRegister = 0
 		self.instructions = []
+		self.stackPointer = 0
+		
+	def addConstantRegister(self, value):
+		self.registers["c" + str(self.nextConstantRegister)] = Register("c" + str(self.nextConstantRegister), "constant", initValue=value)
+		self.nextConstantRegister += 1
+		
+	def addInputRegister(self, valid=lambda x: True):
+		self.registers["i" + str(self.nextInputRegister)] = Register("i" + str(self.nextInputRegister), "input", valid=valid)
+		self.nextInputRegister += 1
+
+	def addOutputRegister(self, value):
+		self.registers["o" + str(self.nextOutputRegister)] = Register("o" + str(self.nextOutputRegister), "output", initValue=value)
+		self.nextOutputRegister += 1
+		
+	def addVariableRegister(self, value):
+		self.registers["v" + str(self.nextVariableRegister)] = Register("v" + str(self.nextVariableRegister), "variable", initValue=value)
+		self.nextVariableRegister += 1
+		
+	def assign(self, register, value):
+		if register not in list(self.registers.keys()):
+			raise ValueError("No register named " + str(register) + " exists in this genetic object")
+		else:
+			self.registers[register].assign(value)
+			
+	def addInstruction(self, insType, terms, output):
+		inp = []
+		for term in terms:
+			if term not in list(self.registers.keys()):
+				inp.append(term)
+			else:
+				inp.append(self.registers[term])
+		self.instructions.append(insType(inp, self.registers[output]))
+	
+	def step(self, inputs={}):
+		for inp in list(inputs.keys()):
+			self.registers[inp].assign(inputs[inp], inputOp=True)
+		self.instructions[self.stackPointer].op()
+		self.stackPointer += 1
+			
+	def __str__(self):
+		result = "GeneticSolver("
+		for register in list(self.registers.keys()):
+			result = result + str(register) + ":" + str(self.registers[register].value) + ","
+		result += ")"
+		return result
+			
+if __name__=="__main__":
+	test = GeneticSolver()
+	test.addConstantRegister(1)
+	test.addInputRegister(valid=lambda x: x < 5)
+	test.addOutputRegister(0)
+	test.addVariableRegister(1)
+	test.addInstruction(AddInstruction, ["v0", 1], "o0")
+	print(test)
+	test.step({"i0": 1})
+	print(test)
--- solver/Register.py
+++ solver/Register.py
@@ -4,20 +4,20 @@ class Register:
 	'''
 	
 	def __init__(self, name, registerType, initValue=None, valid=lambda x: True):
-		if(registerType not in ["input", "variable", "constant", "calculation", "output"]):
+		if(registerType not in ["input", "constant", "variable", "output"]):
 			raise ValueError("Unacceptable register type " + str(registerType))
-		elif(initValue == None and registerType in ["calculation", "output"]):
-			raise ValueError("Calculation and output variables must be initialized")
+		elif(initValue == None and registerType in ["variable", "output"]):
+			raise ValueError("Variable and output registers must be initialized")
 		else:
 			self.name = name
 			self.registerType = registerType
 			self.value = initValue
 			self.valid = valid
 			
-	def assign(self, value):
+	def assign(self, value, inputOp=False):
 		if not self.valid(value):
 			raise ValueError("Value " + str(value) + " is not valid for register " + str(self.name))
-		elif self.registerType in ["constant", "variable"]:
+		elif self.registerType in ["constant", "input"] and not inputOp:
 			raise ValueError("Cannot write to register " + str(self.name) + " of type " + str(self.registerType))
 		else:
 			self.value = value
