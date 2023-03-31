2968b96ed31e03a597133de52d0e03a6c1bcf50b
Zedrick Serson <18zws@queensu.ca>
2023-03-30 20:15:55 -0400

    Implement mutate and crossover operators for MS

--- solver/GeneticSolver.py
+++ solver/GeneticSolver.py
@@ -7,9 +7,10 @@ class GeneticSolver:
 	as pertains to solving mazes.
 	'''
 	
-	def __init__(self, length):
+	def __init__(self, length, string=""):
 		self.length = length
-		self.string = ""
+		self.string = string
+		self.fitness = 0
 	
 	def init(self):
 		if self.string != "":
@@ -28,7 +29,21 @@ class GeneticSolver:
 		mutatedString = list(self.string)
 		mutatedString.insert(point, mutatedSection)
 		del mutatedString[point]
-		self.string = mutatedString
+		return GeneticSolver(self.length, mutatedString)
+		
+	def crossover(self, parent2):
+		point1 = random.randrange(0, len(self.string))
+		point2 = random.randrange(0, len(self.string))
+		mutationLength = 3
+		while(random.randrange(0, 2) == 1):
+			mutationLength += 1
+		string1 = list(self.string)
+		string2 = list(parent2.string)
+		print(string1[point1:point1+mutationLength])
+		print(string2[point2:point2+mutationLength])
+		string1[point1:point1+mutationLength], string2[point2:point2+mutationLength] = \
+			string2[point2:point2+mutationLength], string1[point1:point1+mutationLength]
+		return GeneticSolver(self.length, "".join(string1)), GeneticSolver(self.length, "".join(string2))
 		
 	def lengthen(self, newLength):
 		for i in range(newLength - self.length):
@@ -38,3 +53,7 @@ class GeneticSolver:
 			
 if __name__=="__main__":
 	test = GeneticSolver(15)
+	test.init()
+	test2 = GeneticSolver(15)
+	test2.init()
+	child1, child2 = test.crossover(test2)
